424a1da5c58390d25b7e40d41059f2b85fd0b6e8
Jorge Luis Guevara Alejandro <61109@contraloria.gob.pe>
2024-10-30 17:24:24 -0500

    changes about layout dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-
     <>
-      <span className="text-5xl">Hola mundo</span>
+      <span className="text-5xl">Login</span>
     </>
   );
 }
